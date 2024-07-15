c82215c56abe0d31390194226529f384fd35a599
jieun <122079153+Cjieun@users.noreply.github.com>
2024-07-15 15:36:58 +0900

    footer 수정

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -38,15 +38,16 @@ watch(showModal, (newValue) => {
         <figure>
           <img src="@/assets/images/finevo_logo_white.png" />
         </figure>
-        <div class="footer_data body2 bg_normal">
+        <div class="footer_data bg_normal">
           <span @click="openModal('privacy_policy')">개인정보 처리방침</span>
           <!--<span @click="openModal('terms_of_service')">이용약관</span>-->
         </div>
         <div class="footer_info_box bg_normal">
           <div class="body4 footer_info">
+            <div>상호명 : 피네보 주식회사</div>
             <div>
               <span>대표 : 심정석</span>
-              <span>상호 : 피네보 주식회사 852-81-01151</span>
+              <span>사업자 번호 : 852-81-01151</span>
             </div>
             <div>
               주소 : 서울특별시 서초구 서초대로 77길 55 에이프로 스퀘어(워워크)
@@ -70,18 +71,20 @@ watch(showModal, (newValue) => {
 .footer_container {
   background: $BACKGROUND_DARK;
   width: 100%;
-  height: 540.84px;
+  height: 576.84px;
   padding: 68px 0;
   display: flex;
   justify-content: center;
   align-items: center;
 
   section {
+    width: 1060px;
     display: flex;
     flex-direction: column;
     gap: 84px;
     overflow: hidden;
     white-space: nowrap;
+    align-items: flex-start;
   }
 }
 
@@ -99,6 +102,9 @@ watch(showModal, (newValue) => {
     gap: 44px;
     line-height: 28px;
     cursor: pointer;
+    @extend .body2;
+    font-weight: 800;
+    letter-spacing: -0.5px;
   }
 
   .footer_info_box {
@@ -117,6 +123,7 @@ watch(showModal, (newValue) => {
 }
 
 .footer_copyright {
+  width: 100%;
   text-align: center;
   padding: 8px;
   line-height: 24px;
@@ -124,11 +131,11 @@ watch(showModal, (newValue) => {
 
 @media (max-width: 767px) {
   .footer_container {
-    height: 602.84px;
+    height: 626.84px;
     padding: 68px 24px;
 
     section {
-      gap: 84px;
+      width: 100%;
     }
   }
 
@@ -136,20 +143,19 @@ watch(showModal, (newValue) => {
     .footer_info_box {
       .footer_info {
         gap: 10px;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
 
         div {
           display: flex;
           flex-direction: column;
           gap: 10px;
           white-space: normal;
-          min-width: 345px;
+          max-width: 345px;
         }
       }
     }
-  }
-
-  .footer_copyright span {
-    font-size: 12px;
   }
 }
 </style>
